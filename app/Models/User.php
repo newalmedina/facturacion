1d1cad01d716db3ae8927b1a4c023144b2ccaed3
newal medina <ing.newal.medina@gmail.com>
2025-07-02 22:00:43 +0200

    no message

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,13 +6,14 @@ namespace App\Models;
 
 use Filament\Panel\Concerns\HasAvatars;
 use Filament\Panel;
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Storage;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable, HasAvatars;
 
@@ -59,7 +60,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
-   
+
     public function country()
     {
         return $this->belongsTo(Country::class);
