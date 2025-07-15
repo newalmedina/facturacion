751b04cd14ec6b74cfe8faf77dbd79880579c3e4
newal medina <ing.newal.medina@gmail.com>
2025-07-15 08:10:37 +0200

    no message

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,10 +17,10 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->job(new SendTestEmailJob)->everyMinute();
         // $schedule->command('inspire')->hourly();
-        $schedule->command('email:send-cron-test')
+        /*$schedule->command('email:send-cron-test')
             //->everyMinute()
             ->dailyAt('05:00')
-            ->appendOutputTo(storage_path('logs/cron.log'));
+            ->appendOutputTo(storage_path('logs/cron.log'));*/
 
 
         /*$schedule->command(HelloCron::class, ['--no-ansi'])
