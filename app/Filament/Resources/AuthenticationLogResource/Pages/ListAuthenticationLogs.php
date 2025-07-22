46ba08b1f7b01849151f75455d930669837a1232
IM3-238\im3-div6 <newal.medina@ingenieros-im3.com>
2025-07-22 09:53:43 +0200

    no message

--- app/Filament/Resources/AuthenticationLogResource/Pages/ListAuthenticationLogs.php
+++ app/Filament/Resources/AuthenticationLogResource/Pages/ListAuthenticationLogs.php
@@ -13,7 +13,7 @@ class ListAuthenticationLogs extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            //Actions\CreateAction::make(),
         ];
     }
 }
