694d834fe8e76e22a2d7b0d2a4f11c7c3a65416f
newal medina <ing.newal.medina@gmail.com>
2025-08-09 03:28:03 +0200

    no message

--- app/Filament/Pages/Backups.php
+++ app/Filament/Pages/Backups.php
@@ -40,4 +40,6 @@ class Backups extends BaseBackups
             ->success()
             ->send();
     }
+
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\BackupDownloadController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,3 +20,8 @@ Route::get('/', function () {
 
     return view('welcome');
 });
+
+
+Route::middleware('auth')->get('/admin/backups/download/{filepath}', [BackupDownloadController::class, 'download'])
+    ->where('filepath', '.*')
+    ->name('filament.backups.download');
