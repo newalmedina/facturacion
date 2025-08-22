b4665b303658139c3d5ceae9c0ab2e9e20086d3c
IM3-238\im3-div6 <newal.medina@ingenieros-im3.com>
2025-08-22 15:13:58 +0200

    dsadsa

--- resources/views/livewire/front-booking.blade.php
+++ resources/views/livewire/front-booking.blade.php
@@ -28,11 +28,12 @@
                            <div id="calendar-container" wire:ignore>
                                 <!-- calendario -->
                             </div>
-                        
+                            
                         </div>
-
-                    
+                        
+                        
                         <div class="col-12  mb-3 pt-5">
+                            <input class="form-control w-100" type="date" id="date" wire:model.live="selectedDate" class="form-control d-none">
                             <div class="mb-3">
                                 <label class="form-label">Selecciona un trabajador</label>
                                 <select wire:model.live="worker_id" class="form-control w-100">
@@ -246,7 +247,7 @@
   
 
 @push('scripts')
-<script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+{{-- <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
 <script src="https://cdn.jsdelivr.net/npm/flatpickr/dist/l10n/es.js"></script>
 
 <script>
@@ -284,7 +285,7 @@
             }
         });
     }
-</script>
+</script> --}}
 @endpush
 
 
