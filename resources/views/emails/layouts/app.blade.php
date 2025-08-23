d34221b2179b74dd26bcb3757ede3f8d8b24a7ed
newal medina <ing.newal.medina@gmail.com>
2025-08-23 20:13:12 +0200

    no message

--- resources/views/emails/layouts/app.blade.php
+++ resources/views/emails/layouts/app.blade.php
@@ -100,20 +100,48 @@
 
                     <!-- Contenido dinámico -->
                     @yield('content')
-
+                    <tr>
+                        <td style="padding: 30px 40px; color: #333333; font-size: 16px; line-height: 1.5;">
+                            <h2>Hola {{ $appointment->worker->name }},</h2>
+                    
+                            <p>
+                                <b>{{ ucfirst($appointment->requester_name) }}</b>
+                                acaba de solicitar una cita para el día <strong>{{ $appointment->date->format('d/m/Y') }}</strong>,
+                                desde las <strong>{{ $appointment->start_time->format('H:i') }}</strong> hasta las <strong>{{ $appointment->end_time->format('H:i') }}</strong>.
+                            </p>
+                            <ul>
+                                <li>Teléfono: <strong>{{ $appointment->requester_phone }}</strong></li>
+                                <li>Email: <strong>{{ $appointment->requester_email  }}</strong></li>
+                                <li>Peinado: <strong>{{ $appointment->item->name . ", " . $appointment->item->total_price }} €</strong></li>
+                    
+                            </ul>
+                            
+                            <hr>
+                            <p>Por favor revisa tu agenda y confirma la disponibilidad en el siguiente enlace:</p>
+                            <p>
+                                <a href="{{ url('admin/appointments/' . $appointment->id . '/edit') }}">Ver cita</a>
+                            </p>
+                        </td>
+                    </tr>
                     
                     <!-- Pie de página -->
                     <tr>
-                        @php
+                        <td>
+                            @php
                         $contactForm = \App\Models\CmsContent::findBySlug('contact-form');
                     @endphp
-                            <p>PAra cualquier otro tipo de información no dudes en ponerte en contacto<a 
-                                id="floating-whatsapp-btn" 
-                                target="_blank" 
-                                href="https://wa.me/{{ preg_replace('/\D/', '', $contactForm->whatsapp_url) }}" 
-                                title="Chatear por WhatsApp">
-                                {{ $contactForm->whatsapp_url}}
-                             </a>.</p>
+                            <p>
+                                Si necesitas más información, no dudes en contactarnos a través de 
+                                <a 
+                                    id="floating-whatsapp-btn" 
+                                    target="_blank" 
+                                    href="https://wa.me/{{ preg_replace('/\D/', '', $contactForm->whatsapp_url) }}" 
+                                    title="Chatear por WhatsApp">
+                                    WhatsApp
+                                </a>. ¡Estaremos encantados de ayudarte!
+                            </p>
+                            
+                        </td>
                     </tr>
                     <tr>
                         <td class="footer">
