b6c38884492d73920f7e93fd83343669abc7f169
newal medina <ing.newal.medina@gmail.com>
2025-09-06 04:17:12 +0200

    eliminando backups antiguos

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -20,6 +20,7 @@ class Kernel extends ConsoleKernel
         // Ejecutar aviso de citas pendientes todos los días a las 5:00 AM
         $schedule->command('appointments:send-pending-to-workers')->dailyAt('05:00');
         $schedule->command('locations:translate')->dailyAt('07:00');
+        $schedule->command('erase:backup_old')->dailyAt('06:00');
         // $schedule->command('backup:run')->dailyAt('05:00');
         // $schedule->command('backup:run --only-db --disable-notifications')->dailyAt('1:30')->environments(['production']);
         // $schedule->job(new SendTestEmailJob)->everyMinute();
