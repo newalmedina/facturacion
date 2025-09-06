675b9e0e941cf6a650e8ee0e9cbd7146fac97f10
newal medina <ing.newal.medina@gmail.com>
2025-09-06 03:14:11 +0200

    no message

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -4,10 +4,23 @@ namespace App\Providers\Filament;
 
 use Tapp\FilamentAuthenticationLog\FilamentAuthenticationLogPlugin;
 use App\Filament\AvatarProviders\AutenticatedUserAvatar;
-use App\Filament\CustomWidgets\AppointmentStats;
-use App\Filament\Resources\AppointmentResource;
-use App\Filament\Resources\AppointmentTemplateResource;
-use App\Filament\Widgets\CalendarWidget;
+use App\Filament\CustomWidgets\GananciasMensualesChart;
+use App\Filament\CustomWidgets\GananciasStats;
+use App\Filament\CustomWidgets\GastosPercentPieChart;
+use App\Filament\CustomWidgets\GastosPieChart;
+use App\Filament\CustomWidgets\OtherExpensesChart;
+use App\Filament\CustomWidgets\OtherExpensesStats;
+use App\Filament\CustomWidgets\ProductosMasVendido;
+use App\Filament\CustomWidgets\ProductosMasVendidoPorcentaje;
+use App\Filament\CustomWidgets\ProductosMenosVendido;
+use App\Filament\CustomWidgets\ProductosMenosVendidoPorcentaje;
+use App\Filament\CustomWidgets\VentasStats;
+use App\Filament\Pages\Settings\Settings;
+use App\Filament\CustomWidgets\VentasMensualesChart;
+use App\Filament\CustomWidgets\VentasPorVendedorPercentPieChart;
+use App\Filament\CustomWidgets\VentasPorVendedorPieChart;
+use App\Filament\Pages\Backups;
+use App\Filament\Pages\CalendarPage;
 use App\Http\Middleware\AuthenticateAndCheckActive;
 use App\Models\Setting;
 use Filament\Http\Middleware\Authenticate;
@@ -29,12 +42,13 @@ use Filament\Facades\Filament;
 use Filament\Navigation\MenuItem;
 use Filament\Pages\Auth\PasswordReset\RequestPasswordReset;
 use Filament\Support\Enums\MaxWidth;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Storage;
+use Outerweb\FilamentSettings\Filament\Plugins\FilamentSettingsPlugin;
 use Saade\FilamentFullCalendar\FilamentFullCalendarPlugin;
+use ShuvroRoy\FilamentSpatieLaravelBackup\FilamentSpatieLaravelBackupPlugin;
 
-class PersonalPanelProvider extends PanelProvider
+class AdminPanelProvider extends PanelProvider
 {
 
     public function panel(Panel $panel): Panel
@@ -44,36 +58,59 @@ class PersonalPanelProvider extends PanelProvider
             ->default()
             ->sidebarCollapsibleOnDesktop()
             ->maxContentWidth(MaxWidth::Full)
-            ->id('personal')
-            ->path('personal')
+            ->id('admin')
+            ->path('admin')
             ->login()
             ->passwordReset(RequestPasswordReset::class)
             ->colors([
-                'primary' => '#581177',     // Lavanda claro 581177
-                'secondary' => '#ecc4f4',   // Púrpura profundo
-                'success' => '#10b981',     // Verde esmeralda (mantengo este para no romper alerts)
-                'danger' => '#dc3545',      // Rojo clásico
-                'warning' => '#ffc107',     // Amarillo warning
-                'info' => '#0dcaf0',        // Azul info
-                'light' => '#f5f5f5',       // Gris claro de fondo
-                'dark' => '#000000',        // Negro
+
+                //'primary' => Color::Amber,
+                'primary' => Color::Blue,      // Azul similar a Bootstrap primary (#0d6efd)
+                'secondary' => Color::Zinc,
+                'success' => Color::Emerald,   // Verde success (#198754)
+                'danger' => Color::Red,        // Rojo danger (#dc3545)
+                'warning' => Color::Yellow,    // Amarillo warning (#ffc107)
+                'info' => Color::Sky,          // Azul info (#0dcaf0)
+                'light' => Color::Gray,         // gris claro aproximado
+                'dark' => Color::Slate,
             ])
-            ->resources([])
             ->defaultAvatarProvider(AutenticatedUserAvatar::class)
+            ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
+            ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->plugins([
+                FilamentSettingsPlugin::make()
+                    ->pages([
+                        Settings::class,
+                    ]),
+                FilamentAuthenticationLogPlugin::make(),
+                // FilamentSpatieLaravelBackupPlugin::make()
+                FilamentSpatieLaravelBackupPlugin::make()
+                    ->usingPage(Backups::class)->authorize(fn(): bool => auth()->user()->email === 'el.solitions@gmail.com'),
                 FilamentFullCalendarPlugin::make()->config(
                     []
                 ),
             ])
             ->pages([
                 Pages\Dashboard::class,
-                \App\Filament\Pages\Profile::class, // 👈 registra tu página
             ])
             // ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
                 Widgets\AccountWidget::class,
-                AppointmentStats::class,
-                CalendarWidget::class,
+                //Widgets\FilamentInfoWidget::class,
+                VentasStats::class,
+                OtherExpensesStats::class,
+                VentasMensualesChart::class,
+                OtherExpensesChart::class,
+                GananciasMensualesChart::class,
+                VentasPorVendedorPieChart::class,
+                VentasPorVendedorPercentPieChart::class,
+                GastosPieChart::class,
+                GastosPercentPieChart::class,
+                ProductosMasVendido::class,
+                ProductosMasVendidoPorcentaje::class,
+                ProductosMenosVendido::class,
+                ProductosMenosVendidoPorcentaje::class,
+                // VentasVsGastosPorDiaChart::class
             ])
             ->middleware([
                 EncryptCookies::class,
@@ -89,24 +126,6 @@ class PersonalPanelProvider extends PanelProvider
             ->authMiddleware([
                 Authenticate::class,
                 AuthenticateAndCheckActive::class,
-            ])
-            ->userMenuItems([
-                MenuItem::make('profile')
-                    ->label('Perfil')
-                    ->url(fn() => \App\Filament\Pages\Profile::getUrl(panel: 'personal'))
-                    ->icon('heroicon-o-user'),
-
-                MenuItem::make('admin')
-                    ->label('Ir a Administración')
-                    ->url(url('/admin'))
-                    ->icon('heroicon-o-cog')
-                    ->visible(fn() => Auth::check() && Auth::user()->can_admin_panel), // ✅ visibilidad condicional
-
-                MenuItem::make('home')
-                    ->label('Ir a la Home')
-                    ->url(url('/'))
-                    ->icon('heroicon-o-globe-alt')
-                    ->openUrlInNewTab(),
             ]);
 
 
@@ -124,6 +143,8 @@ class PersonalPanelProvider extends PanelProvider
             }
         }
 
+
+
         return $panel;
     }
 
@@ -131,18 +152,24 @@ class PersonalPanelProvider extends PanelProvider
     public function boot()
     {
         Filament::serving(function () {
-
             Filament::registerUserMenuItems([
                 'profile' => MenuItem::make()
                     ->label('Perfil')
                     ->url(route('filament.admin.pages.profile')) // Aquí también agregamos la URL
                     ->icon('heroicon-o-user'),
+                'personal' => MenuItem::make()
+                    ->label('Ir a al panel trabajador')
+                    ->url(url('/personal')) // tu landing page
+                    ->icon('heroicon-o-briefcase'), // ícono opcional
+                // ->openUrlInNewTab(), // opcional: abre en nueva pestaña
                 'home' => MenuItem::make()
                     ->label('Ir a la Home')
                     ->url(url('/')) // tu landing page
                     ->icon('heroicon-o-globe-alt') // ícono globo terrestre
                     ->openUrlInNewTab(), // opcional: abre en nueva pestaña
+
             ]);
         });
     }
 }
+    
