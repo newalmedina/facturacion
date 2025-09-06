bc818bd32c460c6a8b76f66ec090c02c86fb5162
newal medina <ing.newal.medina@gmail.com>
2025-09-06 04:19:25 +0200

    dsadsa

--- app/Console/Commands/EraseBackupOld.php
+++ app/Console/Commands/EraseBackupOld.php
@@ -11,6 +11,7 @@ class EraseBackupOld extends Command
     protected $signature = 'erase:backup_old';
     protected $description = 'Elimina los backups antiguos de más de 20 días';
 
+    public $totalDias = 10;
     public function handle()
     {
         $this->info('Buscando backups antiguos para eliminar...');
@@ -33,7 +34,7 @@ class EraseBackupOld extends Command
             $lastModified = Carbon::createFromTimestamp(filemtime($fullPath));
 
             // Si el archivo tiene más de 20 días
-            if ($lastModified->diffInDays($now) > 20) {
+            if ($lastModified->diffInDays($now) >$this->totalDias) {
                 unlink($fullPath);
                 $this->info("Eliminado: {$file}");
                 $deletedCount++;
